58663775d1565b4d70021001e7939c5439e96e2f
Hendrik Jacobus Barnard <heinrichbarnard@hotmail.com>
2018-06-11 19:01:08 +0200

    Added get user

--- src/main/java/mallekoppie/springboot/poc/UserController.java
+++ src/main/java/mallekoppie/springboot/poc/UserController.java
@@ -1,14 +1,22 @@
 package mallekoppie.springboot.poc;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mallekoppie.springboot.poc.model.User;
 
 @RestController
 public class UserController {
 	
 	@RequestMapping("/")
-	public String Index(){
+	public String getIndex(){
 		return "Test";
+	}
+	
+	@GetMapping(path="/user")
+	public User getUser(){
+		return new User("myName", "mySurname");
 	}
 
 }
