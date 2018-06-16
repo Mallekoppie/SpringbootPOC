74e1a794cb7fef37001336dfd17a6a3dae429793
Hendrik Jacobus Barnard <heinrichbarnard@hotmail.com>
2018-06-16 11:30:45 +0200

    We have rest service in another project and split platform contracts and implementations

--- cb.base/src/main/java/cb/platform/BaseApplication.java
+++ cb.base/src/main/java/cb/platform/BaseApplication.java
@@ -1,11 +1,8 @@
 package cb.platform;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import cb.platform.services.test.UserService;
 
 @SpringBootApplication
 @ComponentScan
--- cb.service/src/main/java/cb/platform/services/test/UserService.java
+++ cb.service/src/main/java/cb/platform/services/test/UserService.java
@@ -1,15 +1,22 @@
 package cb.platform.services.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cb.platform.contract.ILogger;
 
 @RestController
 @Configuration
 public class UserService {
 
+	@Autowired
+	ILogger _logger;
+	
 	@GetMapping(path = "/user")
 	public String getTest() {
+		_logger.WriteInfo("Called getTest method");
 		return "another user service";
 	}
 }
